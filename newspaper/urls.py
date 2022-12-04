ba23a7676ebef86be9159afb6a281a97f35437d3
Vadim Kosnovskiy <kvmltkv@gmail.com>
2022-12-04 17:42:48 +0200

    Add paths accounts, debug, include content.urls and STATIC_URL in urls.py newspaper

--- newspaper/urls.py
+++ newspaper/urls.py
@@ -13,9 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from newspaper import settings
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+    path("admin/", admin.site.urls),
+    path("", include("content.urls", namespace="content")),
+    path("accounts/", include("django.contrib.auth.urls")),
+    path("__debug__/", include("debug_toolbar.urls")),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
