d1b8c0cc247153fae49997d2d8dbcb30302cf348
Vadim Kosnovskiy <kvmltkv@gmail.com>
2022-12-04 18:01:53 +0200

    Add index in views.py

--- content/views.py
+++ content/views.py
@@ -1,1 +1,26 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from .models import Redactor, Newspaper, Topic, Article
+
+
+@login_required
+def index(request):
+    """View function for the home page of the site."""
+
+    num_redactors = Redactor.objects.count()
+    num_newspapers = Newspaper.objects.count()
+    num_topics = Topic.objects.count()
+    num_articles = Article.objects.count()
+
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
+
+    context = {
+        "num_redactors": num_redactors,
+        "num_newspapers": num_newspapers,
+        "num_topics": num_topics,
+        "num_articles": num_articles,
+        "num_visits": num_visits + 1,
+    }
+
+    return render(request, "content/index.html", context=context)
