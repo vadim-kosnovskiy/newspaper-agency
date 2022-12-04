c8e989ec23e75802b2e1cc0fb2343699ae9e9921
Vadim Kosnovskiy <kvmltkv@gmail.com>
2022-12-04 19:50:50 +0200

    Add paths articles/, topics/, newspapers/, redactors/ to content.urls.py

--- content/urls.py
+++ content/urls.py
@@ -2,10 +2,15 @@ from django.urls import path
 
 from content.views import (
     index,
+    TopicListView, NewspaperListView, RedactorListView, ArticleListView,
 )
 
 urlpatterns = [
     path("", index, name="index"),
+    path("articles/", ArticleListView.as_view(), name="article-list"),
+    path("topics/", TopicListView.as_view(), name="topic-list"),
+    path("newspapers/", NewspaperListView.as_view(), name="newspaper-list"),
+    path("redactors/", RedactorListView.as_view(), name="redactor-list"),
 ]
 
 app_name = "content"
