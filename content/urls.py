fcb1330ea96b9b47066921eaa16e1db80644af8e
Vadim Kosnovskiy <kvmltkv@gmail.com>
2022-12-05 11:18:50 +0200

    Add paths to newspaper-detail, newspaper-create, newspaper-update, newspaper-delete in in content.urls

--- content/urls.py
+++ content/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from content.views import (
     index,
     TopicListView, NewspaperListView, RedactorListView, ArticleListView,
-    RedactorDetailView, RedactorCreateView, RedactorDeleteView, RedactorUpdateView,
+    RedactorDetailView, RedactorCreateView, RedactorDeleteView, RedactorUpdateView, TopicDeleteView, TopicUpdateView,
+    TopicCreateView,
 )
 
 urlpatterns = [
@@ -26,6 +27,18 @@ urlpatterns = [
         name="topic-delete"
     ),
     path("newspapers/", NewspaperListView.as_view(), name="newspaper-list"),
+    path("newspapers/<int:pk>/", NewspaperDetailView.as_view(), name="newspaper-detail"),
+    path("newspapers/create/", NewspaperCreateView.as_view(), name="newspaper-create"),
+    path(
+        "newspapers/<int:pk>/update/",
+        NewspaperUpdateView.as_view(),
+        name="newspaper-update"
+    ),
+    path(
+        "newspapers/<int:pk>/delete/",
+        NewspaperDeleteView.as_view(),
+        name="newspaper-delete"
+    ),
     path("redactors/", RedactorListView.as_view(), name="redactor-list"),
     path(
         "redactors/<int:pk>/", RedactorDetailView.as_view(), name="redactor-detail"
