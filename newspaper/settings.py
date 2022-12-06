6c20f41cf6520d7aeef58f9a91f35ed8717416e7
Vadim Kosnovskiy <kvmltkv@gmail.com>
2022-12-06 16:19:07 +0200

    SECRET KEY protect in .env file, add .env to .gitignore

--- newspaper/settings.py
+++ newspaper/settings.py
@@ -1,3 +1,5 @@
+import os
+import dotenv
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -8,7 +10,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--*71g+3)fca=kkt+f75*w@*v)^4n$zsd04p318v5tqc32kku@)'
+dotenv_file = os.path.join(BASE_DIR, ".env")
+if os.path.isfile(dotenv_file):
+    dotenv.load_dotenv(dotenv_file)
+
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
